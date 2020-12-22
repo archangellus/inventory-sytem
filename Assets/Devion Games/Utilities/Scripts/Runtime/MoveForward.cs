0383c949cc4e13819c6c141d5d4b5d254177f940
archangellus <archangellus@users.noreply.github.com>
2020-12-22 20:17:47 +0200

    Sync with NEW-RPG
    Merged

--- Assets/Devion Games/Utilities/Scripts/Runtime/MoveForward.cs
+++ Assets/Devion Games/Utilities/Scripts/Runtime/MoveForward.cs
@@ -13,14 +13,18 @@ namespace DevionGames
         private bool m_AutoDestruct = true;
         [SerializeField]
         private float m_DestructDelay = 10f;
-        private Rigidbody m_Rigidbody;
+        [SerializeField]
+        private GameObject hitEffect= null;
 
+
+        private Rigidbody m_Rigidbody;
+        private Collider m_Collider;
 
 
         private void Start()
         {
             this.m_Rigidbody = GetComponent<Rigidbody>();
-
+            this.m_Collider = GetComponent<Collider>();
             this.transform.parent = null;
             if (this.m_AutoDestruct)
                 Destroy(gameObject, this.m_DestructDelay);
@@ -38,5 +42,16 @@ namespace DevionGames
             this.m_Rigidbody.velocity = transform.forward * m_Speed; 
         }
 
+        private void OnCollisionEnter(Collision collision)
+        {
+            this.m_Collider.enabled = false;
+            this.m_Rigidbody.velocity = Vector3.zero;
+            this.m_Rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+            transform.position = collision.GetContact(0).point;
+            if (hitEffect != null)
+                Instantiate(hitEffect, transform.position, Quaternion.identity);
+
+            Destroy(gameObject);
+        }
     }
 }
